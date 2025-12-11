bacb8764d1044ba4ea3cdee11acfd8f02d04181a
edybostina <egbostina@gmail.com>
2025-12-11 16:43:23 +0200

    fixed double comment issue

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -9,26 +9,6 @@
  * Include this file to use the matrix library.
  *
  * @author edybostina
- * @date 2025-09-23 at 20:30
- * @version 0.1.8
- * @note This is a work in progress and may not be fully functional.
- *       The library is intended for educational purposes and may
- *       not be suitable for production use.
- *
- * @license MIT License
- */
-
-/**
- * @file matrix.hpp
- * @brief Definition of the templated matrix<T> class.
- *
- * This header declares the matrix<T> class, a lightweight and flexible
- * C++ matrix implementation supporting basic linear algebra operations,
- * element access, and utility functions (zeros, ones, eye, etc.).
- *
- * Include this file to use the matrix library.
- *
- * @author edybostina
  * @date 2025-12-11
  * @version 0.2.0
  * @note This is a work in progress and may not be fully functional.
